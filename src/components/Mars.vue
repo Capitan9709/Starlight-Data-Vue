e5052594bf5bb2a05ec665bd5e5597ef5af0526c
PTMARKETING01 <ptmarketing01@MEGACENTRAL.local>
2023-04-26 17:32:21 +0200

    se ha creado lapagina mars

---
+++ src/components/Mars.vue
@@ -0,0 +1,43 @@
+<script setup>
+import Head from './Head.vue';
+import Foot from './Foot.vue';
+
+
+</script>
+
+<template>
+  <Head/>
+    
+  <section id="cuerpo" class="pt-10">
+    <div class="about">
+      <section class=" py-12 px-4">
+        <div class="max-w-5xl mx-auto flex flex-col justify-center items-center">
+          <h2 class="text-4xl font-bold mb-4">Mars Weather</h2>
+
+          <iframe src='https://mars.nasa.gov/layout/embed/image/mslweather/' width='1000' height='570'  scrolling='no' frameborder='0'></iframe>
+
+        </div>
+      </section>
+    </div>
+    <div>
+      <Foot/>
+    </div>
+  </section>
+</template>
+
+
+<style scoped>
+#cuerpo {
+  position: relative; /* agrega posicionamiento relativo para que el pseudo-elemento tenga un contexto de posicionamiento */
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  background-image: url(../assets/marteTiempo.jpg);
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -8,11 +8,13 @@ import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Database from './components/Database.vue'
 import Rover from './components/Rover.vue'
+import Mars from './components/Mars.vue'
 
 const routes = [
     { path: '/', component: Home, name: 'Home' },
     { path: '/about', component: About, name: 'About' },
     { path: '/database', component: Database, name: 'Database' },
+    { path: '/mars', component: Mars, name: 'Mars' },
     { path: '/rover', component: Rover, name: 'Rover' },
 ]
 
