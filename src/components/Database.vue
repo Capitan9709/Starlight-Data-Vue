b200e203d966ba216a4c46e018333cb75adc0b4c
PTMARKETING01 <ptmarketing01@MEGACENTRAL.local>
2023-04-26 10:58:33 +0200

    se ha mejorado la pagina database

--- src/components/Database.vue
+++ src/components/Database.vue
@@ -1,9 +1,9 @@
 <script setup>
 import Head from './Head.vue';
 import Foot from './Foot.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { db } from '@/firebase.js';
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
 // variables para la fecha de ayer
 const date = new Date()
@@ -11,8 +11,6 @@ const year = date.getFullYear()
 const month = ('0' + (date.getMonth() + 1)).slice(-2)
 const day = ('0' + (date.getDate()-1)).slice(-2)
 const yesterdayDate = `${year}-${month}-${day}`
-// console.log(yesterdayDate)
-
 
 // variables para la imagen del dia
 const imagenDiaAyer = ref('');
@@ -23,17 +21,19 @@ const descripcionImagenDiaAyer = ref('');
 // variables para los asteroides
 const asteroidesAyer = ref([]);
 
+// variables para las imagenes
+const imagenesTodas = ref([]);
+const currentIndex = ref(0);
+
 // saca los datos de la imagen de ayer de la base de datos
 const apodCollection = doc(db, "APODs", yesterdayDate);
 const apodSnap = await getDoc(apodCollection);
 
 if (apodSnap.exists()) {
-  
   imagenDiaAyer.value = apodSnap.data().imagen;
   tituloImagenDiaAyer.value = apodSnap.data().titulo;
   fechaimagenDiaAyer.value = apodSnap.data().fecha;
   descripcionImagenDiaAyer.value = apodSnap.data().descripcion;
-
 } else {
   console.log("No existe el documento");
 }
@@ -43,15 +43,37 @@ const asteriodCollection = doc(db, "Asteroids", yesterdayDate);
 const asteroidSnap = await getDoc(asteriodCollection);
 
 if (asteroidSnap.exists()) {
-  
   asteroidesAyer.value = asteroidSnap.data().asteroides;
-
-//   console.log("asteroides",asteroidesAyer.value);
 } else {
   console.log("No existe el documento");
 }
 
+// saca los datos de todas las imagenes de la base de datos
+const imagenesCollection = collection(db, "APODs");
+const imagenesSnap = await getDocs(imagenesCollection);
+
+if (!imagenesSnap.empty) {
+  imagenesSnap.forEach((doc) => {
+    imagenesTodas.value.push(doc.data());
+  });
+  imagenesTodas.value.reverse();
+} else {
+  console.log("No existe el documento");
+}
+
+const getImages = computed(() => {
+  return imagenesTodas.value.slice(0, currentIndex.value + 2);
+})
+
+const hasMoreImages = computed(() => {
+  return currentIndex.value < imagenesTodas.value.length - 2;
+})
+
+function loadMoreImages() {
+  currentIndex.value += 2;
+}
 </script>
+
 
 <template>
     <Head/>
@@ -88,6 +110,29 @@ if (asteroidSnap.exists()) {
 
                         </li>
                         </ul>
+                    </div>
+                </div>
+                <!-- all images -->
+                <div class="my-10 w-full flex flex-col justify-center items-center">
+                    <h2 class="text-2xl font-bold text-center">All Images from Database</h2>
+                    <div class="flex flex-col items-center justify-center my-4">
+                        <ul class="flex flex-row flex-wrap gap-3 justify-center items-start">
+                            <li v-for="(imagen, index) in getImages" :key="index" class="text-center bg-indigo-950 p-3 rounded-md w-1/3">
+                                <div class="flex flex-row justify-between border-b-2 border-gray-700">
+                                  <p><b>Title:</b> {{ imagen.titulo }}</p>
+                                  <p><b>Date:</b> {{ imagen.fecha }}</p>  
+                                </div>  
+                                <div class="flex justify-center items-center py-2 px-2 bg-cover h-auto w-full bg-indigo-950 rounded-md">
+                                    <img class="bg-cover bg-center rounded-md" alt="imagen astronomica del dia" :src="imagen.imagen">
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="flex justify-center mt-6">
+                          <button v-if="hasMoreImages" @click="loadMoreImages" class="py-1 px-3  bg-indigo-950 rounded font-semibold hover:bg-indigo-700 hover:shadow-lg hover:shadow-indigo-500/50 transition duration-200 ease-out hover:ease-in">
+                            Load More <i class="fa-solid fa-plus"></i>
+                          </button>
+                          <p v-else class="font-semibold underline decoration-indigo-500">Nothing to Load</p>
+                        </div>
                     </div>
                 </div>
         </div>
